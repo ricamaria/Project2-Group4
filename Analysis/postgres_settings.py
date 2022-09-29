5ff06e5e09d975cd5949889665f0f324dd829d0b
rajiv_wick@iinet.net.au <rajiv_wick@iinet.net.au>
2022-09-29 23:04:22 +0800

    added postgres settings file to limit server login details within database_final juypter notebook file

---
+++ Analysis/postgres_settings.py
@@ -0,0 +1,7 @@
+postgresql = { 'user' : 'postgres',
+                'passwrd' : 'postgres',
+                'host' : 'localhost',
+                'port' : 5432,
+                'db' : 'econ_db'
+
+}
